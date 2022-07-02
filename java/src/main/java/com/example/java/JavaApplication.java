26bb3e1887d5a1ae1469d9283071c12db397c8ac
TangLeDaily <470193496@qq.com>
2022-07-02 20:21:34 +0800

    GS Vesion1.0

---
+++ java/src/main/java/com/example/java/JavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// this is springboot orinss
+@SpringBootApplication
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplication.class, args);
+    }
+
+}
